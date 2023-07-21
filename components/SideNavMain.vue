232336701853087eaf0a383434ea403c00471493
desmondorix <desmondorixk@gmail.com>
2023-07-21 23:44:33 +0700

    digtok axios

--- components/SideNavMain.vue
+++ components/SideNavMain.vue
@@ -1,5 +1,5 @@
 <template>
-      <div 
+    <div 
         id="SideNavMain" 
         :class="route.fullPath === '/' ? 'lg:w-[310px]' : 'lg:w-[220px]'" 
         class="fixed z-20 bg-white pt-[70px] h-full lg:border-r-0 border-r w-[75px] overflow-auto"
@@ -15,56 +15,60 @@
             <div class="border-b lg:ml-2 mt-2" />
 
             <div class="lg:block hidden text-xs text-gray-600 font-semibold pt-4 pb-2 px-2">
-                 Suggested accounts
-            </div>    
+                Suggested accounts
+            </div>
 
             <div class="lg:hidden block pt-3" />
 
-            <div
+
+            <div 
                 v-if="$generalStore.suggested" 
                 v-for="sug in $generalStore.suggested"
             >
                 <div @click="isLoggedIn(sug)" class="cursor-pointer">
                     <MenuItemFollow :user="sug" />
                 </div>
-            </div>            
-                
+            </div>
+
+
             <button class="lg:block hidden text-[#F02C56] pt-1.5 pl-2 text-[13px]">
                 See all
-            </button>  
+            </button>
 
-            <div class="border-b lg:ml-2 mt-2" />
-            <div class="lg:block hidden text-xs text-gray-600 font-semibold pt-4 pb-2 px-2">
-                 Following accounts
-            </div>  
-            
-            <div class="lg:hidden block pt-3" />
+            <div v-if="$userStore.id">
+                <div class="border-b lg:ml-2 mt-2" />
 
-            <div
-                v-if="$generalStore.following" 
-                v-for="fol in $generalStore.following"
-            >
-                <div @click="isLoggedIn(fol)" class="cursor-pointer">
-                    <MenuItemFollow :user="fol" />
+                <div class="lg:block hidden text-xs text-gray-600 font-semibold pt-4 pb-2 px-2">
+                    Following accounts
                 </div>
-            </div>  
-            
-                
-            <button class="lg:block hidden text-[#F02C56] pt-1.5 pl-2 text-[13px]">
-                See more
-            </button>  
 
+                <div class="lg:hidden block pt-3" />
+
+                <div 
+                    v-if="$generalStore.following" 
+                    v-for="fol in $generalStore.following"
+                >
+                    <div @click="isLoggedIn(fol)" class="cursor-pointer">
+                        <MenuItemFollow :user="fol" />
+                    </div>
+                </div>
+
+                <button class="lg:block hidden text-[#F02C56] pt-1.5 pl-2 text-[13px]">See more</button>
+
+            </div>
             <div class="lg:block hidden border-b lg:ml-2 mt-2" />
+
             <div class="lg:block hidden text-[11px] text-gray-500">
-                <div class="pt-4 px-2">About Newsroom TikTik Shop Contact Careers ByteDance</div>
-                <div class="pt-4 px-2">TikTik for Good Advertise Developers Transparency TikTik Rewards TikTik Browse TikTik Embeds</div>
+                <div class="pt-4 px-2">About Newsroom DigTok Shop Contact Careers ByteDance</div>
+                <div class="pt-4 px-2">DigTok for Good Advertise Developers Transparency DigTok Rewards DigTok Browse DigTok Embeds</div>
                 <div class="pt-4 px-2">Help Safety Terms Privacy Creator Portal Community Guidelines</div>
-                <div class="pt-4 px-2">© 2023 TikTik</div>
+                <div class="pt-4 px-2">© 2023 DigTok</div>
             </div>
 
             <div class="pb-14"></div>
+        </div>
+
     </div>
-      </div>
 </template>
 
 <script setup>
@@ -80,4 +84,3 @@ const isLoggedIn = (fol) => {
     setTimeout(() => router.push(`/profile/${fol.id}`), 200)
 }
 </script>
-
